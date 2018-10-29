70ea2c31b8bc1b4940a0ce4f9965783266edb28f
Sri Nath <sri.developer731@gmail.com>
2018-10-29 18:41:16 -0400

    Connect to api

--- avatar-dwldr.js
+++ avatar-dwldr.js
@@ -1,3 +1,35 @@
 const request = require('request');
 const fs = require('fs');
+const env = require('dotenv').config();
+const token = process.env.API_ACCESS_TOKEN;
+const user = 'SrChip15';
+
+const getRepoContributors = function(repoOwner, repoName, callback) {
+  const options = {
+    url:`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request',
+      'access_token': token
+    }
+  };
+
+  if (repoOwner && repoName) {
+    // owner and repo validated
+    request(options, function(err, response, body) {
+      if (response.statusCode === 200) {
+        console.log(`Connected to GitHub...`);
+        callback(err, body);
+      }
+    });
+  }
+};
+
+const isSuccess = function(err, body) {
+  if (err) throw err;
+  // console.log(body);
+};
+
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+getRepoContributors('facebook', 'react', isSuccess);
 
